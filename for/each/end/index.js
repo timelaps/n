5899b5b3ed76753955155fd7bb9f6c7a4f2959b3
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-07 22:59:24 -0500

    updated where logic was pulling from

--- for/each/end/index.js
+++ for/each/end/index.js
@@ -1,6 +1,6 @@
 var u, isArrayLike = require('@timelaps/is/array-like'),
     lastIndex = require('../../../last/key'),
-    forEachEndBase = require('../../../from/to');
+    forEachEndBase = require('../../../from/to/end');
 module.exports = function forEach(array, fn) {
     return isArrayLike(array) ? forEachEndBase(array, fn, 0, lastIndex(array), 1) : u;
 };
