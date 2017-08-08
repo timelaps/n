3f3a9bd8cf99a0c3353b90b7c9699d384f2e4799
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-07 23:15:29 -0500

    added other methods to the group export

--- for/own/right/index.js
+++ for/own/right/index.js
@@ -1,6 +1,6 @@
 var keys = require('../../../keys');
 var forEachRight = require('../../each/right');
-module.exports = function forOwnRight(array, fn) {
+module.exports = function forOwnRight(object, fn) {
     forEachRight(keys(object), function (key) {
         fn(object[key], key, object);
     });
