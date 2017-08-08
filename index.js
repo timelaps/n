fab0649afd177678279ddc68c1fb5f92d3fda42e
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-07 20:28:10 -0500

    added package info

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    forEach: require('./for/each'),
+    forIn: require('./for/in'),
+    forInHas: require('./for/in/has'),
+    forOwn: require('./for/own'),
+    forEachEnd: require('./for/each/end'),
+    collectNonEnumProps: require('./collect-non-enum-props'),
+    keys: require('./keys'),
+    keysAll: require('./keys/all'),
+    keysOwn: require('./keys/own'),
+    keysGenerator: require('./keys/generator'),
+    keysNative: require('./keys/native')
+};
