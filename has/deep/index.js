2e01e3e39249eb736b7e02bdc4565815007332fa
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-08 22:24:45 -0500

    updated deep has check

--- has/deep/index.js
+++ has/deep/index.js
@@ -1,7 +1,8 @@
 var hasShallow = require('../shallow');
 var getDeep = require('../../get');
 module.exports = function hasDeep(target, path) {
-    var sub = path.slice(0, path.length - 1);
+    var lastIdx = path.length - 1;
+    var sub = path.slice(0, lastIdx);
     var got = getDeep(target, sub);
-    return hasShallow(target, path[length - 1]);
+    return hasShallow(target, path[lastIdx]);
 };
