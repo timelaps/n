5f907aa71edde2833a58e004d36246c6a6ababfa
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-12 09:03:58 -0500

    redirected where for each comes from

--- for/each/index.js
+++ for/each/index.js
@@ -1,6 +1,1 @@
-var isArrayLike = require('@timelaps/is/array-like');
-var forEachEnd = require('./end');
-module.exports = function forEach(array, fn) {
-    var i = 0, length = array.length;
-    for (; i < length; i++) fn(array[i], i, array);
-};
+module.exports = require('@timelaps/hacks/for/each');
