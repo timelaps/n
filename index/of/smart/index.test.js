e594bf57dda3a13d337c633779cbf3c3ae7913fa
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-15 09:59:31 -0500

    updated deps and smart index to use from to instead of map after addition of hasownproperty check

--- index/of/smart/index.test.js
+++ index/of/smart/index.test.js
@@ -1,18 +1,15 @@
 var smartIndexOf = require('.');
 var b = require('@timelaps/batterie');
-var map = require('../../../map');
+var fromTo = require('../../../from/to');
 b.describe('smartIndexOf', function () {
     b.expect(smartIndexOf).toBeFunction();
     b.it('finds elements just like indexOf', function (t) {
         t.expect(smartIndexOf([1, 3, 5, 7, 9], 9)).toBe(4);
     });
     b.it('will break if the elements are not sorted', function (t) {
-        var base = [4];
-        var longlist = base.concat.apply(base, map({
-            length: 20
-        }, function (value, index) {
-            return [1, 2, 3, 5, 6, 7, 8, 9];
-        }));
+        var longlist = [4].concat(fromTo(function (index, memo) {
+            return memo.concat([1, 2, 3, 5, 6, 7, 8, 9]);
+        }, [], 0, 20, 1));
         t.expect(smartIndexOf(longlist, 4)).toBe(-1);
     });
 });
