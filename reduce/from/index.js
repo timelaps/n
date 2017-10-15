b17b8614e7c02e01a11c9d65691cf8e886be1075
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-10-15 18:59:50 -0500

    updated reduce from to include an object passthrough for the 4th arg

--- reduce/from/index.js
+++ reduce/from/index.js
@@ -1,12 +1,11 @@
 var reduction = require('../');
 var generateIndices = require('@timelaps/fn/generator/indices');
 var returnsTrue = require('@timelaps/returns/true');
-var returnsSecond = require('@timelaps/returns/second');
 module.exports = reduceFrom;
 
 function reduceFrom(validator_, generate_, get, transform_) {
     // no object passed back
-    var transform = transform_ || returnsSecond;
+    var transform = transform_ || passThroughObject;
     var validator = validator_ ||returnsTrue;
     var generate = generate_ || generateIndices;
     return function reducer(obj, iteratee, memo) {
@@ -15,5 +14,11 @@ function reduceFrom(validator_, generate_, get, transform_) {
         function access(key) {
             return obj[key];
         }
+    };
+}
+
+function passThroughObject(object, iterator) {
+    return function (memo, value, key) {
+        return iterator(memo, value, key, object);
     };
 }
